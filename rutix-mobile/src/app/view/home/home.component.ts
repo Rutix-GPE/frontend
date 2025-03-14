102575aa3e9b0b6a9c67c48dd2a65845484795fb
Pierre <pvmacci@gmail.com>
2025-03-14 15:46:56 +0100

    card + button add

--- rutix-mobile/src/app/view/home/home.component.ts
+++ rutix-mobile/src/app/view/home/home.component.ts
@@ -52,7 +52,6 @@ export class HomePage implements OnInit {
 
 
   ngOnInit(): void {
-    this.updateClock();
     this.authService.currentUser$.subscribe(user => {
       this.currentUser = user;
       this.loadQuestions();
@@ -180,27 +179,7 @@ export class HomePage implements OnInit {
     };
   }
 
-  updateClock() {
-    const hourHand = document.querySelector('.hour-hand') as HTMLElement;
-    const minuteHand = document.querySelector('.minute-hand') as HTMLElement;
-    const secondHand = document.querySelector('.second-hand') as HTMLElement;
 
-    function setClock() {
-      const now = new Date();
-      const seconds = now.getSeconds();
-      const minutes = now.getMinutes();
-      const hours = now.getHours();
-
-      const secondDegrees = ((seconds / 60) * 360) + 90;
-      const minuteDegrees = ((minutes / 60) * 360) + ((seconds / 60) * 6) + 90;
-      const hourDegrees = ((hours / 12) * 360) + ((minutes / 60) * 30) + 90;
-
-      secondHand.style.transform = `rotate(${secondDegrees}deg)`;
-      minuteHand.style.transform = `rotate(${minuteDegrees}deg)`;
-      hourHand.style.transform = `rotate(${hourDegrees}deg)`;
-    }
-    setInterval(setClock, 1000);
-  }
 
 
   cancelTask() {
